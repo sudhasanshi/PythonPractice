23e8e60af9407e10f49978840585cfa9e0cf855a
sudha sanshi <35460293+sudhasanshi@users.noreply.github.com>
2024-01-25 20:14:42 +0530

    Create arthmetic.py

---
+++ arthmetic.py
@@ -0,0 +1,17 @@
+#length of string
+text="sudha"
+length=len(text)
+print(length)
+num1=2.5
+num2=5.2
+#addition of two numbers
+sum=num1 + num2
+print(f"the sum of {num1} and {num2} is {sum}")
+sub=num1-num2
+print(f"the diff of {num1} and {num2} is {sub}")
+mul=num1*num2
+print(f"the product of {num1} and {num2} is {mul}")
+div=num1//num2
+print("the division of num1 {} and num2 {} is {}".format(num1, num2, div))
+mod=num1%num2
+print(f"the mod of {num1} and {num2} is", mod)
