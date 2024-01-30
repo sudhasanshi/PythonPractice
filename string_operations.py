c62ad5a09ee70aa24ec0af4920267dfffd64b314
sudha sanshi <35460293+sudhasanshi@users.noreply.github.com>
2024-01-30 10:02:07 +0530

    Update string_operations.py

--- string_operations.py
+++ string_operations.py
@@ -34,3 +34,36 @@ print(sentence.split()) #['hello', 'All', 'good!']
 print(sentence.split(" ", 1)) #['hello', 'All good!']
 #reverse split
 print(sentence.rsplit(" ", 1))#['hello All', 'good!']
+
+Basic build-in methods:
+1.	lower(): It is used to convert string into lowercase
+var = “Hello.World”)
+var1 = var.lower()
+print(var1)
+2.	upper(): Used to convert string into uppercase
+var = “Hello.World”)
+print(var.upper())
+3.	startswith(): used to check whether string is starts with pattern
+var = “Hello.World”)
+if var.startswith(“He”):
+	print(“starts with pattern”)
+else:
+	print(“starts with different pattern”)
+4.	endswith(): used to check whether string ends with specific pattern or not
+5.	var = “Hello.World”)
+if var.endswith(“He”):
+	print(“ends with pattern”)
+else:
+	print(“sends with different pattern”)
+6.	split(): used to split the string based on delimiter
+var = “hello.wor.ld”
+val, val1, val2 = var.split(‘.’)
+above string is divided into 3 parts “hello”, “wor”, “ld”
+val, val1= var.split(‘.’, 1)
+above is splitted only one time since in function, 1 is passed. (Hello, wor.ld)
+val, val1 = var.rsplit(‘.’, 1)
+splitted from reverse order (Hello.wor, ld)
+7.	replace: Used to replace the one character by another character
+var = “hello.wor.ld”
+var1 – var.replace(‘.’, ’/’)
+print(var1) ------- Hello/wor/ld
