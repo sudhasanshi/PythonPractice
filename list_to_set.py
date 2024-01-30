75a85d351a986c3b017de077de258f0cbb359fe1
sudha sanshi <35460293+sudhasanshi@users.noreply.github.com>
2024-01-30 23:12:33 +0530

    Create list_to_set_convert.py

---
+++ list_to_set.py
@@ -0,0 +1,9 @@
+#converting list to set
+list_day=["mon", "tue", "wed"]
+print(list_day)
+print(set(list_day))
+
+#converting set to list
+set_mon={"jan", "feb", "mar"}
+print(set_mon)
+print(list(set_mon))
