c34e5c3a34c1cc000e270db354d37d7edd4b26a8
sudha sanshi <35460293+sudhasanshi@users.noreply.github.com>
2024-01-30 19:41:44 +0530

    Create 01_string.py

---
+++ 01_string.py
@@ -0,0 +1,19 @@
+msg="Hello all! good morning"
+
+print(msg.lower()) #hello all! good morning
+
+print(msg.upper()) #HELLO ALL! GOOD MORNING
+
+print(msg[0]) #h
+
+print(msg[-1])#g
+
+print(msg[0:5])#hello
+
+print(msg.split("!"))#['hello all', ' good morning']
+
+firstName="sudha"
+
+lastName="sanshi"
+
+print(firstName+ " " +lastName)#sudha sanshi
