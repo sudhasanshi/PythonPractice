67f819b62f98b9fc01c246f5ab6ce56b0b4c20e5
sudha sanshi <35460293+sudhasanshi@users.noreply.github.com>
2024-02-01 15:27:31 +0530

    Create 01_assignment.py

---
+++ 01_assignment.py
@@ -0,0 +1,5 @@
+Assignment : Write a script to read the notice, license file and add the content of each file to the file notice_license.txt. The notice or license file may be started with notice or license
+before appending the content to notice_license.txt file add the relative path --> This can also be said as the automation script. Add this to the automation scripts question
+
+To fetch notice and license file from project directory (notice.txt, license.txt, notice.md etc) and the add the contents of this file along with relative path to the single file.
+Purpose of writing this script ->  For internal usage
