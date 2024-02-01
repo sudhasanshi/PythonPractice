8e88413d55a7ecd447acd2e647bf56e246f1ac23
sudha sanshi <35460293+sudhasanshi@users.noreply.github.com>
2024-02-01 22:36:24 +0530

    Update 01_fetch_dictonary.py

--- 01_fetch_dictonary.py
+++ 01_fetch_dictonary.py
@@ -1,3 +1,17 @@
+sub= {"birds": ["sparrow", "kingfisher", "parrot", "macaw", "hummingbird"], "types": {"intigers": [1, 2, 3, 4, 5],
+    "floats": None, "alphabets": {"vowels": ['a', 'e', 'i', 'o', 'u'], "consonants": "bcdfghjklmnpqrstvwxyz"}},
+      "mydict" :{"avengers": ["hulk", "antman", "spiderman"], "xmen": ["raven", "wovelirin"]}}
+for s in sub:
+    print(s) # it will print all the keys of dictionary
+for index, keys in enumerate(sub):
+    print(keys,index) # it will print keys and index values
+for key in sub.keys():
+    print(key) # it will print all keys one by one
+for value in sub.values():
+    print(value) # it will print all values 
+for key, value in sub.items():
+    print(key,value)# it will print both all keys and values
+
 mydict={}
 print("my empty dictionary is ", mydict)
 mydict={"name": "abc", "id": 1}
