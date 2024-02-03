9956b3aba886209ce6990a67a37027e22428c107
Ubuntu <ubuntu@ip-172-31-41-235.ap-south-1.compute.internal>
2024-02-03 08:24:45 +0000

    excel to json

---
+++ sample-dir/convert_excel_to_json_file.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+filename="sample.xlsx"
+output="sample.json"
+
+
+def excel_to_json(name, json_file):
+    df =  pd.read_excel(name, sheet_name="Sheet1")
+    df_stripped = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+    df_stripped.to_json(json_file, orient='records', indent=4)
+    print("json file created successfully")
+
+   
+
+
+excel_to_json(filename, output)
+
---
+++ sample-dir/read_excel_file.py
@@ -0,0 +1,4 @@
+import pandas as pd
+filename="sample.xlsx"
+res=pd.read_excel(filename)
+print(res)
