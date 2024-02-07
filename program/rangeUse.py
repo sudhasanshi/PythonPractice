b85c110b5cd24d62cf6ccddfa5d98f26b4d4e159
sudha sanshi <35460293+sudhasanshi@users.noreply.github.com>
2024-02-07 22:19:51 +0530

    Create rangeUse.py

---
+++ program/rangeUse.py
@@ -0,0 +1,18 @@
+for i in range(10):
+  print(i)
+
+
+name="sudha"
+for i in name:
+    print(i)
+
+for i in "sudha":
+    print(i)
+
+for i in ["sudha", 1,'a']:
+    print(i)    
+
+output :  
+sudha
+1
+a  
