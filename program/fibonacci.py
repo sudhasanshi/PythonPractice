59fbc5afabb68e458de4b20c6f4c05fc4e6ef998
sudha sanshi <35460293+sudhasanshi@users.noreply.github.com>
2024-04-15 22:58:34 +0530

    Update fibonacci.py

--- program/fibonacci.py
+++ program/fibonacci.py
@@ -14,3 +14,18 @@ def fibo(n):
 
     
 fibo(n)
+
+
+n = int(input("Provide number to find Fibonacci series: "))
+a = 0
+b = 1
+
+print(f"{a}\n{b}")
+
+i = 2
+while i < n:
+    fibo = a + b
+    a = b
+    b = fibo
+    print(fibo)
+    i += 1
